f72666cbb9ff47b0b3a237b71935dcd29a36b38e
Abhishek Kumar <mr.kumar.abhishek@email.com>
2016-12-23 01:17:56 +0530

    put cross on every user click

--- js/main.js
+++ js/main.js
@@ -35,6 +35,30 @@ TICTACTOE.events = {
 		$("#1").click(function(){
 			$(this).html("X");
 		});
+		$("#2").click(function(){
+			$(this).html("X");
+		});
+		$("#3").click(function(){
+			$(this).html("X");
+		});
+		$("#4").click(function(){
+			$(this).html("X");
+		});
+		$("#5").click(function(){
+			$(this).html("X");
+		});
+		$("#6").click(function(){
+			$(this).html("X");
+		});
+		$("#7").click(function(){
+			$(this).html("X");
+		});
+		$("#8").click(function(){
+			$(this).html("X");
+		});
+		$("#9").click(function(){
+			$(this).html("X");
+		});
 	}
 }
 
